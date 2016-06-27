7eff1052ea81354566020387232a23fa50fae0d6
Carlton Win8 <carlton.win8@gmail.com>
2016-06-26 21:22:20 -0700

    recieve json data from NYT after request via NYT api

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function loadData() {
     $nytElem.text("");
 
     // load streetview
-    var debug = false;
+    var debug = true;
     if (debug) {
       switch (0) {
         case 1:
@@ -34,8 +34,22 @@ function loadData() {
     var img = '<img class="bgimg" src="' + ajaxUrl + '">';
     $body.append(img);
 
-
     // YOUR CODE GOES HERE!
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({'api-key': "57575961577542d386f9f4665b401e00",
+      'q': city});
+    $.getJSON( url, function( data ) {
+      $nytHeaderElem.text('New York Times Articles About' + city);
+      $.each( data.response.docs, function( idx, doc ) {
+        var a = '<a href="' + doc.web_url + '">' + doc.headline.main + '</a>';
+        var p = '<p>' + doc.snippet + '</p>';
+        var li = '<li class="article" id="' + doc._id +'">' + a + p + '</li>';
+        $nytElem.append(li);
+      });
+    })
+    .fail(function() {
+      $nytHeaderElem.text('Failed! Could not load the New York Times Articles');
+    });
 
     return false;
 };
